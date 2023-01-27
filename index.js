1483791823d0a3c02027e3a29cb10ad167aa692c
lufepamadev <lufepama.dev@gmail.com>
2023-01-28 00:00:06 +0100

    Add delete functionality

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 //Input instances
 const itemInput = document.querySelector('.item-input');
 const addItemBtn = document.querySelector('.input-btn');
-const itemList = document.querySelector('.item-list');
+var itemList = document.querySelector('.item-list');
+var elementsList = document.querySelectorAll('.item-list li')
 
 //Main app instances
 const appMainContainer = document.querySelector('.app-main-container')
 
-
-//Modal instances
+//Modal and basic operation instances
 const openModalBtn = document.querySelector('.open-modal-btn')
+const deleteBtn = document.querySelector('.delete-btn')
 const modalCancelBtn = document.querySelector('.cancel-btn')
 const myModalContainer = document.querySelector('.my-modal-visible')
 
+
+//Global variables
+var temporalList = []
+var temporalIdList = []
+var trackEvents = []
 
 // Add item functionality
 addItemBtn.addEventListener('click', () => {
@@ -20,6 +26,8 @@ addItemBtn.addEventListener('click', () => {
         return;
     }
     const task = document.createElement('li');
+    const idGenerated = generateRandomId()
+    if (idGenerated) task.setAttribute('id', idGenerated)
     task.classList.add('item-text-no-selected');
     task.innerHTML = itemInput.value;
 
@@ -28,7 +36,12 @@ addItemBtn.addEventListener('click', () => {
     myModalContainer.classList.add('no-display')
     appMainContainer.classList.remove('on-brightness-class')
     openModalBtn?.removeAttribute('disabled')
+    itemList = document.querySelector('.item-list')
+    elementsList = document.querySelectorAll('.item-list li')
 
+    //Regenerate click event in new items added in the list
+    updateItemList()
+    updateDeleteAction()
 });
 
 //Open modal to add item
@@ -50,3 +63,79 @@ modalCancelBtn.addEventListener('click', () => {
     openModalBtn?.removeAttribute('disabled')
     myModalContainer.classList.add('no-display')
 })
+
+//Handle click action in items
+elementsList.forEach((item) => {
+    item.addEventListener('click', () => {
+        item.classList.remove('item-text-no-selected')
+        item.classList.add('item-text-selected')
+        const title = item.innerHTML
+        const id = item.id
+        const itemData = { title, id }
+        temporalList.push(itemData)
+        temporalIdList.push(id)
+    })
+})
+
+//Handle delete click action
+deleteBtn.addEventListener('click', () => {
+    console.log('list', temporalIdList)
+    elementsList.forEach((item) => {
+        if (temporalIdList.includes(item.id)) {
+            try {
+                itemList.removeChild(item)
+                updateItemList()
+            } catch (error) {
+                console.log('error', error)
+            }
+        }
+    })
+
+})
+
+
+
+
+//--->Methods<-----
+
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//Generate randomg stringid which is then used in 'li' items.
+function generateRandomId(length = 10) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+//Allows add click event in updated list
+function updateItemList() {
+    elementsList.forEach((item) => {
+        item.addEventListener('click', () => {
+            item.classList.remove('item-text-no-selected')
+            item.classList.add('item-text-selected')
+            const title = item.innerHTML
+            const id = item.id
+            const itemData = { title, id }
+            temporalList.push(itemData)
+            temporalIdList.push(id)
+        })
+    })
+}
+
+function updateDeleteAction() {
+    deleteBtn.addEventListener('click', () => {
+        elementsList.forEach((item) => {
+            if (temporalIdList.includes(item.id)) {
+                try {
+                    itemList.removeChild(item)
+                    updateItemList()
+                } catch (error) {
+                    console.log('error', error)
+                }
+            }
+        })
+
+    })
+}
