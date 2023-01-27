048172ce037bbeb5c55c0312d46e726f7595ed9c
lufepamadev <lufepama.dev@gmail.com>
2023-01-27 15:03:41 +0100

    Add modal interactivity

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-const itemInput = document.querySelector('.itemInput');
-const addItemBtn = document.querySelector('.addItemBtn');
-const itemList = document.querySelector('.itemList');
+const itemInput = document.querySelector('.item-input');
+const addItemBtn = document.querySelector('.input-btn');
+const itemList = document.querySelector('.item-list');
+
+const openModalBtn = document.querySelector('.open-modal-btn')
+const modalCancelBtn = document.querySelector('.cancel-btn')
+
+const myModalContainer = document.querySelector('.my-modal-visible')
 
 
-// Agregar tarea
-addItemBtn.addEventListener('click', function () {
-    if (itemInput.value === '') {
+// Add item functionality
+addItemBtn.addEventListener('click', () => {
+    if (itemInput?.value === '') {
         alert('Ingresa una tarea!');
         return;
     }
 
     const task = document.createElement('li');
-    task.classList.add('item');
+    task.classList.add('item-text-no-selected');
     task.innerHTML = itemInput.value;
 
     itemList.appendChild(task);
 
     itemInput.value = '';
 });
+
+//Open modal to add item
+openModalBtn.addEventListener('click', () => {
+    openModalBtn.setAttribute('disabled', 'disabled')
+    openModalBtn.setAttribute('disabled', 'disabled')
+    myModalContainer.classList.remove('no-display')
+})
+
+//Manges close modal
+modalCancelBtn.addEventListener('click', () => {
+    openModalBtn?.removeAttribute('disabled')
+    myModalContainer.classList.add('no-display')
+
+})
